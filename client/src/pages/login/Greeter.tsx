0ac076aa7b2f290bd3b1b9809c13bce002704c92
idiot-kitto <46341496+idiot-kitto@users.noreply.github.com>
2021-11-01 17:24:56 +0900

    design: element 가운데 배치

--- client/src/pages/login/Greeter.tsx
+++ client/src/pages/login/Greeter.tsx
@@ -5,13 +5,25 @@ const Greeter = () => {
   const reactArray = "JDevBook".split("");
   
   return (
-    <Content>
-    {reactArray.map((item, index):any => (
-        <span key={index}>{item}</span>
-    ))}
-    </Content>
+    <>
+      <Wrapper>
+        <div>우리들의 공간</div>
+        <Content>
+        {reactArray.map((item, index):any => (
+            <span key={index}>{item}</span>
+        ))}
+        </Content>
+      </Wrapper>
+    </>
   )
 }
+
+const Wrapper = styled.span`
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+  font-size: 50px;
+`;
 
 const Animation = keyframes`
   0% { opacity: 0; transform: translateY(-30px) skewY(10deg) skewX(10deg) rotateZ(30deg); filter: blur(10px); }
@@ -22,9 +34,10 @@ const Animation = keyframes`
 
 const Content = styled.span`
   width: 50vw;
-  font-size:50px;
+  font-size: 50px;
   display: inline-block;
   span {
+      
       display: inline-block;
       opacity: 0;
       animation-name: ${Animation};
--- client/src/pages/login/LoginBox.tsx
+++ client/src/pages/login/LoginBox.tsx
@@ -1,9 +1,18 @@
 import React from 'react';
+import styled from 'styled-components';
 
 const LoginBox = () => {
     return (
-        <span>로그인 박스 예정</span>
+        <Wrapper>로그인 박스 예정</Wrapper>
     )
 }
+
+const Wrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+  width: 50vw;
+`;
 
 export default LoginBox;
