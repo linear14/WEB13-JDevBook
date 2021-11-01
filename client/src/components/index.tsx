24b9ac584923630e97d88df51d273392b0e261d5
shinn338 <shinn338@naver.com>
2021-11-01 15:21:11 +0900

    feat: 공통 컴포넌트 Layout 작성
    기능이 포함되지 않은 스타일만 적용
    테스트 용도
    
    Related to: #43

--- client/src/components/index.tsx
+++ client/src/components/index.tsx
@@ -3,5 +3,6 @@ import ChatSideBar from './ChatSideBar';
 import GroupSideBar from './GruopSideBar';
 import InfoSideBar from './InfoSideBar';
 import Gnb from './Gnb';
+import SideBar from './SideBar';
 
-export { AlertSideBar, ChatSideBar, GroupSideBar, InfoSideBar, Gnb };
+export { AlertSideBar, ChatSideBar, GroupSideBar, InfoSideBar, Gnb, SideBar };
--- client/src/pages/HomePage.tsx
+++ client/src/pages/HomePage.tsx
@@ -1,8 +1,31 @@
 import React from 'react';
 import styled from 'styled-components';
+import { Gnb, SideBar, InfoSideBar, ChatSideBar, GroupSideBar } from '../components';
+
+const HomePageContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+const ContentsWrap = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
 
 const HomePage = () => {
-  return ``;
+  return (
+    <HomePageContainer>
+      <Gnb />
+      <ContentsWrap>
+        <SideBar>
+          <InfoSideBar />
+          <GroupSideBar />
+        </SideBar>
+        <SideBar>
+          <ChatSideBar />
+        </SideBar>
+      </ContentsWrap>
+    </HomePageContainer>
+  );
 };
 
 export default HomePage;
