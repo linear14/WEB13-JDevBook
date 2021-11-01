9cbbc3d2c98877b8459668d92aefe1c54f63028a
shinn338 <shinn338@naver.com>
2021-11-01 15:58:30 +0900

    design: 공통 컴포넌트 스타일 변경
    fixed를 추가해 스크롤시에도 고정되게 변경
    
    Related to: #43

--- client/src/components/SideBar.tsx
+++ client/src/components/SideBar.tsx
@@ -1,16 +1,22 @@
 import React from 'react';
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
-const SideBarContainer = styled.div`
-  width: 280px;
+const SideBarContainer = styled.div<SideBarProps>`
+  width: 340px;
   height: calc(100vh - 56px);
+  top: 56px;
+  ${(props) => (props.isLeft ? `left: 0` : `right: 0`)};
   position: fixed;
   display: flex;
   flex-direction: column;
 `;
 
-const SideBar: React.FC = ({ children }) => {
-  return <SideBarContainer>{children}</SideBarContainer>;
+type SideBarProps = {
+  isLeft: boolean;
+};
+
+const SideBar: React.FC<SideBarProps> = ({ isLeft, children }) => {
+  return <SideBarContainer isLeft={isLeft}>{children}</SideBarContainer>;
 };
 
 export default SideBar;
--- client/src/index.tsx
+++ client/src/index.tsx
@@ -2,12 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import './index.css';
-import App from './App';
+// import App from './App';
+import Router from './Router';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <BrowserRouter>
-    <App />
+    <Router />
   </BrowserRouter>,
   document.getElementById('root'),
 );
