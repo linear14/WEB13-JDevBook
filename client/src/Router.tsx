c70c6fbc04e8a5adbddb92debcdf5126a53eb00e
shinn338 <shinn338@naver.com>
2021-11-01 15:26:19 +0900

    feat: SPA Router 코드 작성
    react-router-dom을 이용해 SPA Router 구현
    404 페이지 간단하게 구현
    
    Related to: #43

--- client/src/Router.tsx
+++ client/src/Router.tsx
@@ -1,5 +1,20 @@
 import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { HomePage } from './pages';
 
-const Router = () => {};
+const Router: React.FC = () => {
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route path='/' exact component={HomePage} />
+        <Route path='/*' component={NotFoundPage} />
+      </Switch>
+    </BrowserRouter>
+  );
+};
+
+const NotFoundPage = () => {
+  return <h2>404 Not Found Page</h2>;
+};
 
 export default Router;
