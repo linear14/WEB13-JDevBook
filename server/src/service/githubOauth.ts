a717c6d61c059207e6ec83fd8facef5b91447bed
hsy <reservedcalender@gmail.com>
2021-11-03 02:29:49 +0900

    refactor: 빼먹은 require -> import 수정

--- server/src/service/githubOauth.ts
+++ server/src/service/githubOauth.ts
@@ -1,6 +1,6 @@
+import axios from 'axios';
 const oauth = require('../config/oauth.json');
 const github = process.env.LOCAL_CLIENT ? oauth.githubLocal : oauth.github;
-const axios = require('axios');
 
 const githubOauth = {
     authorizeURL: `https://github.com/login/oauth/authorize?client_id=${github.CLIENT_ID}&redirect_uri=${github.CALLBACK_URL}`,
