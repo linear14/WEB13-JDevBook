bbf62ead9452ae900ab2f0e7de4e86029cb4fb9a
idiot-kitto <46341496+idiot-kitto@users.noreply.github.com>
2021-11-02 14:30:55 +0900

    fix: Merge후 발생한 에러 수정 ( 경로 )

--- client/src/components/common/InfoSideBar.tsx
+++ client/src/components/common/InfoSideBar.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
-import { ProfilePhoto } from '.';
+import ProfilePhoto from '../common/ProfilePhoto';
 
 const InfoSideBarContainer = styled.div`
   height: 200px;
