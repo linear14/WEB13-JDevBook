ad70db538a3d93e5b99e6004c950f8f308f222b3
hsy <reservedcalender@gmail.com>
2021-11-04 00:56:00 +0900

    feat: interface 타입 usergroup, group, chat 추가
    Related to: #42, #56

--- server/src/service/interface.ts
+++ server/src/service/interface.ts
@@ -16,3 +16,23 @@ export interface DBUser {
   updatedAt: Date;
   deletedAt: Date;
 }
+
+export interface DBUserGroup {
+  idx: number;
+  useridx: number;
+  groupidx: number;
+}
+
+export interface Group {
+  idx: number;
+  title: string;
+  description: string;
+  cover: string;
+}
+
+export interface Chat {
+  idx: number;
+  senderidx: number;
+  receiveridx: number;
+  content: string;
+}
