2598c3ccd2a30b99558f5d07f92187e44cfb0d32
hsy <reservedcalender@gmail.com>
2021-11-04 03:17:45 +0900

    feat: client api fetch에 logout 형태는 만듬
    server api와 조절을 잘하여 처리해야할듯하다.
    
    Related to: #12
    Ref: #42

--- client/src/api/fetch.ts
+++ client/src/api/fetch.ts
@@ -2,6 +2,12 @@
 const getData = {
     getusername: () => {
         return fetch('/api/data').then(res => res.json());
+    },
+    logout: () => {
+        fetch('/ouath/logout');
+        // redirect를 oauth/logout에서 해놨는데
+        // client에서 받은 후에 저장해둔 데이터들 다 제거한 이후 (저장한다면?)
+        // 여기서 window.location.href 하는게 맞을듯
     }
 }
 
