06e2664199261d3295afb4697f0331a086ed9b6b
LeeDongHyun <47495592+linear14@users.noreply.github.com>
2021-11-04 15:02:08 +0900

    feat: 사용자 검색 api 추가
    -  /users?keyword=[:keyword]
    - 오류 시 빈 배열 반환

--- server/src/service/dbManager.ts
+++ server/src/service/dbManager.ts
@@ -1,23 +1,34 @@
-import db from "../models";
+import { Op } from 'sequelize';
+import db from '../models';
 
 const dbManager = {
-    sync: async () => {
-        await db.sync({force: false, logging: false}).then(() => {
-            console.log('Connection has been established successfully.');
-          }).catch((error: any) => {
-            console.error('Unable to connect to the database:', error);
-          })
-          
-    },
-
-    getUserdata: async (username: string) => {
-      const [user, created] = await db.models.User.findOrCreate({
-        where: { nickname: username },
-        defaults: { nickname: username }
+  sync: async () => {
+    await db
+      .sync({ force: false, logging: false })
+      .then(() => {
+        console.log('Connection has been established successfully.');
       })
+      .catch((error: any) => {
+        console.error('Unable to connect to the database:', error);
+      });
+  },
 
-      return user.get();
-    }
-}
+  getUserdata: async (username: string) => {
+    const [user, created] = await db.models.User.findOrCreate({
+      where: { nickname: username },
+      defaults: { nickname: username }
+    });
+
+    return user.get();
+  },
+
+  searchUsers: async (keyword: string) => {
+    const users = await db.models.User.findAll({
+      where: { nickname: { [Op.like]: `%${keyword}%` } }
+    });
+
+    return users;
+  }
+};
 
 export default dbManager;
