baf25050bc8e4451f8e43a2abe3361666d2579b0
shinn338 <shinn338@naver.com>
2021-11-08 20:36:59 +0900

    rename: icon-photo.svg 추가

--- client/src/images/index.tsx
+++ client/src/images/index.tsx
@@ -2,5 +2,6 @@ import mainLogo from 'images/main-logo.png';
 import defaultProfile from 'images/default-profile.jpg';
 import defaultCover from 'images/default-profile.jpg';
 import iconSearch from 'images/icon-search.svg';
+import iconPhoto from 'images/icon-photo.svg';
 
-export { mainLogo, defaultProfile, defaultCover, iconSearch };
+export { mainLogo, defaultProfile, defaultCover, iconSearch, iconPhoto };
