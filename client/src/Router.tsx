dd12f241eb9ad313aa376403ce5e9397ded689d4
hsy <reservedcalender@gmail.com>
2021-11-08 23:51:40 +0900

    style: react Router에서 특정 url 진입 막기 실패
    useRecoilValue가 업데이트 되지 않아 맨처음 없는 값인 userData를 가져온다.
    그래서 로그인을 한 상태에서도 home, group 등의 url로 진입 시도를 하면 로그인화면으로 돌아와진다.
    
    Related to: #78

--- client/src/Router.tsx
+++ client/src/Router.tsx
@@ -1,8 +1,33 @@
-import React from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import userEvent from '@testing-library/user-event';
+import React, { useEffect } from 'react';
+import {
+  BrowserRouter,
+  Route,
+  Switch,
+  RouteComponentProps
+} from 'react-router-dom';
+import { useRecoilState, useRecoilValue } from 'recoil';
+import { userData, usersocket } from 'recoil/modal';
 import { GroupPage, HomePage, LoginPage, ProfilePage } from './pages';
 
+// interface AuthorityRouteType {
+//   path: string;
+//   component: React.ComponentType;
+// }
+
+// 로그인하고 새로고침해도 로그인화면으로 가짐;;
+// const AuthorityRoute = ({ component, ...rest }: any) => {
+//   const userdata = useRecoilValue(userData);
+//   if (userdata.login) return <Route {...rest} exact component={component} />;
+//   else return <Route {...rest} exact component={LoginPage} />;
+// };
+
 const Router: React.FC = () => {
+  // const userdata = useRecoilValue(userData);
+  // const socket = useRecoilValue(usersocket);
+  // useEffect(() => {
+  //   socket.emit('name', userdata.name);
+  // }, [userdata]);
   return (
     <BrowserRouter>
       <Switch>
