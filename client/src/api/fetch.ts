1b31e54e8a8252fb7be2303873856575a541c261
LeeDongHyun <47495592+linear14@users.noreply.github.com>
2021-11-08 19:11:07 +0900

    refactor: searchUsers async/await 제거

--- client/src/api/fetch.ts
+++ client/src/api/fetch.ts
@@ -3,7 +3,7 @@ const getData = {
     return fetch('/api/data').then((res) => res.json());
   },
   logout: () => {
-     fetch('/ouath/logout')
+    fetch('/ouath/logout')
       .then((res) => res.json())
       .then((data) => {
         alert(data.message);
@@ -12,9 +12,8 @@ const getData = {
         window.location.href = '/';
       });
   },
-  searchUsers: async (keyword: string) => {
-    const response = await fetch(`/api/users?keyword=${keyword}`);
-    return await response.json();
+  searchUsers: (keyword: string) => {
+    return fetch(`/api/users?keyword=${keyword}`).then((res) => res.json());
   }
 };
 
