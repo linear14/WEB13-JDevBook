fdb940d04f9857419d33769d2807798009c0f949
LeeDongHyun <47495592+linear14@users.noreply.github.com>
2021-11-09 16:26:59 +0900

    feat: 이미지 크기 체크하는 getImageSize 함수 생성

--- client/src/components/HomePage/PostImageBox.tsx
+++ client/src/components/HomePage/PostImageBox.tsx
@@ -2,6 +2,12 @@ import React from 'react';
 import styled, { css } from 'styled-components';
 import { PostImageBoxProps, PostImageBoxUrl } from 'utils/types';
 
+const getImageSize = (url: string) => {
+  const img = new Image();
+  img.src = url;
+  return [url, img.width, img.height];
+};
+
 const FullImageBox = styled.div<PostImageBoxUrl>`
   width: 100%;
   height: 100%;
@@ -13,6 +19,7 @@ const PostImageBox = ({ images }: PostImageBoxProps) => {
   const imageLength = images.length;
 
   if (imageLength === 1) {
+    console.log(getImageSize(images[0]));
     return <FullImageBox imageUrl={images[0]} />;
   }
   return <div></div>;
