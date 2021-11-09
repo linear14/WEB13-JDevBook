1db8631008a032506acf88135fcf012110f9c4df
hsy <reservedcalender@gmail.com>
2021-11-10 00:48:59 +0900

    feat: url 접근 막는데 로그인화면 잠깐 뜬다.
    server에서 로그인 여부 가져오기 전엔 login false로 값을 설정해놔서 잠깐 로그인화면 뜬다.
    그렇다고 localStorage에 저장하면 수정할 수 있기 떄문에 음...
    
    Related to: #78

--- client/src/Router.tsx
+++ client/src/Router.tsx
@@ -1,11 +1,6 @@
 import userEvent from '@testing-library/user-event';
 import React, { useEffect, useState } from 'react';
-import {
-  BrowserRouter,
-  Route,
-  Switch,
-  RouteComponentProps
-} from 'react-router-dom';
+import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
 import { useRecoilState, useRecoilValue } from 'recoil';
 import { userData, usersocket } from 'recoil/store';
 import { GroupPage, HomePage, LoginPage, ProfilePage } from './pages';
@@ -19,24 +14,31 @@ import { GroupPage, HomePage, LoginPage, ProfilePage } from './pages';
 //처음에 login false로 하면 아예 첨부터 넘어갈 수가 없음
 //아래 함수 내부에서 userRecoilValue하면 처음 userData값인 빈 값만 받고 업데이트 안됨
 //일단 Authority.tsx에서 location.href로 옮기는데 좀 느림
-const AuthorityRoute = ({ component, login, ...rest }: any) => {
-  if (login) return <Route {...rest} exact component={component} />;
-  else return <Route {...rest} exact component={LoginPage} />;
-};
+// const AuthorityRoute = ({ component, login, ...rest }: any) => {
+//   if (login) return <Route {...rest} exact component={component} />;
+//   else return <Route {...rest} exact component={LoginPage} />;
+// };
 
 const Router: React.FC = () => {
-  // const userdata = useRecoilValue(userData);
-  // const [login, setLogin] = useState(false));
-  // // const socket = useRecoilValue(usersocket);
-  // useEffect(() => {
-  //   if (userdata.login) setLogin(true);
-  //   else setLogin(false);
-  // }, [userdata]);
+  const [login, setLogin] = useState(false);
+  useEffect(() => {
+    (async () => {
+      const islogin = await fetch('api/islogin').then((res) => res.json());
+      setLogin(islogin);
+      console.log(login);
+    })();
+  }, [login]);
+  // 단점 첨에 false였다가 true가 되어서 로그인 화면 잠깐 보였다가 HomePage가짐
+
   return (
     <BrowserRouter>
       <Switch>
         <Route path="/" exact component={LoginPage} />
-        <Route path="/home" exact component={HomePage} />
+        <Route
+          path="/home"
+          exact
+          render={() => (login ? <HomePage /> : <LoginPage />)}
+        />
         <Route path="/group" exact component={GroupPage} />
         <Route path="/profile/:userId" exact component={ProfilePage} />
         <Route path="/*" component={NotFoundPage} />
--- server/src/routes/api.ts
+++ server/src/routes/api.ts
@@ -40,6 +40,25 @@ router.get('/data', async (req: Request, res: Response, next: NextFunction) => {
   }
 });
 
+router.get('/islogin', (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const { name } = jwt.verify(req.session.jwt, oauth.jwtKey) as {
+      name: string;
+    };
+    if (name === req.session.username) {
+      res.json(true);
+    } else {
+      req.session.destroy((err) => {
+        res.json(false);
+      });
+    }
+  } catch (err) {
+    req.session.destroy((err) => {
+      res.json(false);
+    });
+  }
+});
+
 router.get(
   '/users',
   async (
