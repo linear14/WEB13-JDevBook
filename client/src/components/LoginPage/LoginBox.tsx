1c491365354adcb2d187c77e8e3117aa6837d3d1
idiot-kitto <46341496+idiot-kitto@users.noreply.github.com>
2021-11-10 17:04:58 +0900

    refactor: LoginPage 파일들 리팩토링

--- client/src/components/LoginPage/LoginBox.tsx
+++ client/src/components/LoginPage/LoginBox.tsx
@@ -1,26 +1,32 @@
 import React from 'react';
 import styled from 'styled-components';
-import mainLogo from '../../images/main-logo.png';
+
 import { GithubLoginButton } from 'react-social-login-buttons';
+
+import mainLogo from '../../images/main-logo.png';
 import fetchApi from 'api/fetch';
 import palette from 'theme/palette';
 
-const LoginBox = () => {
-  return (
-    <Box>
-      <Wrapper>
-        <MainLogoElement />
-        <Text>개발자라면 Github 아이디는 가지고 계시죠?</Text>
-        <GithubLoginButton onClick={fetchApi.login} />
-      </Wrapper>
-    </Box>
-  );
-};
+const Box = styled.div`
+  display: flex;
+  justify-content: center;
+  text-align: center;
+
+  width: 35vw;
+  height: 50%;
+  background-color: ${palette.white};
+  border-radius: 50px;
+  
+  margin-right: 5vw;
+  box-shadow: rgba(60, 64, 67, 0.3) 0px 1px 2px 0px,
+    rgba(60, 64, 67, 0.15) 0px 2px 6px 2px;
+`;
 
 const MainLogoElement = styled.img.attrs({
   src: `${mainLogo}`
 })`
   margin-top: 50px;
+
   &:nth-child(1) {
     margin-bottom: 10px;
   }
@@ -30,19 +36,16 @@ const Text = styled.div`
   margin-bottom: 50px;
 `;
 
-const Box = styled.div`
-  display: flex;
-  justify-content: center;
-  text-align: center;
-  width: 35vw;
-  background-color: ${palette.white};
-  height: 50%;
-  border-radius: 50px;
-  margin-right: 5vw;
-  box-shadow: rgba(60, 64, 67, 0.3) 0px 1px 2px 0px,
-    rgba(60, 64, 67, 0.15) 0px 2px 6px 2px;
-`;
-
-const Wrapper = styled.div``;
+const LoginBox = () => {
+  return (
+    <Box>
+      <div>
+        <MainLogoElement />
+        <Text>개발자라면 Github 아이디는 가지고 계시죠?</Text>
+        <GithubLoginButton onClick={fetchApi.login} />
+      </div>
+    </Box>
+  );
+};
 
 export default LoginBox;
