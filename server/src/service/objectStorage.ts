a0cbbada520ecc019457a3acadeb32774b8f1753
hsy <reservedcalender@gmail.com>
2021-11-11 04:30:40 +0900

    feat: objectStorage getObjectlist 확인
    api 생성
    원하는 조건에 따라서만 가져오는 구현까진 NO
    data 받아오는건 OK
    
    Related to: #72

--- server/src/service/objectStorage.ts
+++ server/src/service/objectStorage.ts
@@ -7,6 +7,7 @@ const url: string = storage.url;
 const region: string = storage.region;
 const access_key: string = storage.access_key;
 const secret_key: string = storage.secret_key;
+const default_bucket: string = storage.default_bucket_name;
 
 const S3 = new AWS.S3({
   endpoint: url,
@@ -24,17 +25,23 @@ const objectStorage = {
       CreateBucketConfiguration: {}
     }).promise();
   },
+
   getBucketlist: async () => {
     // { Name: ~, CreationDate: ~}
     const { Buckets } = await S3.listBuckets().promise();
     return Buckets;
   },
+
   deleteBucket: async (bucket_name: string) => {
     await S3.deleteBucket({
       Bucket: bucket_name
     }).promise();
   },
-  uploadObjectfolder: async (object_name: string, bucket_name = 'jdevbook') => {
+
+  uploadObjectfolder: async (
+    object_name: string,
+    bucket_name = default_bucket
+  ) => {
     // object_name = 'sample-folder/'
     if (object_name[object_name.length - 1] !== '/')
       object_name = object_name + '/';
@@ -44,10 +51,11 @@ const objectStorage = {
       Key: object_name
     }).promise();
   },
+
   uploadObjectfile: async (
     object_name: string,
     local_file_path: string,
-    bucket_name = 'jdevbook'
+    bucket_name = default_bucket
   ) => {
     // object_name = 'sample-object'
     // local_file_path = '/tmp/test.txt'
@@ -58,6 +66,79 @@ const objectStorage = {
       ACL: 'public-read', // console.ncloud에서 보기 가능
       Body: fs.createReadStream(local_file_path)
     }).promise();
+  },
+
+  getObjectlist: async (max_keys: number, bucket_name = default_bucket) => {
+    const params = {
+      Bucket: bucket_name,
+      MaxKeys: max_keys
+    };
+    // https://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/S3.html#listObjectsV2-property
+    const data = await S3.listObjectsV2(params).promise();
+    console.log(data);
+
+    // // List All Objects
+    // console.log('List All In The Bucket');
+    // console.log('==========================');
+
+    // while (true) {
+    //   let response = await S3.listObjectsV2(params).promise();
+
+    //   console.log(`IsTruncated = ${response.IsTruncated}`);
+    //   console.log(`Marker = ${response.Marker ? response.Marker : null}`);
+    //   console.log(
+    //     `NextMarker = ${response.NextMarker ? response.NextMarker : null}`
+    //   );
+    //   console.log(`  Object Lists`);
+    //   for (let content of response.Contents) {
+    //     console.log(
+    //       `    Name = ${content.Key}, Size = ${content.Size}, Owner = ${content.Owner.ID}`
+    //     );
+    //   }
+
+    //   if (response.IsTruncated) {
+    //     params.Marker = response.NextMarker;
+    //   } else {
+    //     break;
+    //   }
+    // }
+
+    // // List Top Level Folder And Files
+    // const params2 = {
+    //   Bucket: bucket_name,
+    //   MaxKeys: max_keys,
+    //   Delimiter: '/'
+    // };
+    // console.log('Top Level Folders And Files In The Bucket');
+    // console.log('==========================');
+
+    // while (true) {
+    //   let response = await S3.listObjectsV2(params2).promise();
+
+    //   console.log(`IsTruncated = ${response.IsTruncated}`);
+    //   console.log(`Marker = ${response.Marker ? response.Marker : null}`);
+    //   console.log(
+    //     `NextMarker = ${response.NextMarker ? response.NextMarker : null}`
+    //   );
+
+    //   console.log(`  Folder Lists`);
+    //   for (let folder of response.CommonPrefixes) {
+    //     console.log(`    Name = ${folder.Prefix}`);
+    //   }
+
+    //   console.log(`  File Lists`);
+    //   for (let content of response.Contents) {
+    //     console.log(
+    //       `    Name = ${content.Key}, Size = ${content.Size}, Owner = ${content.Owner.ID}`
+    //     );
+    //   }
+
+    //   if (response.IsTruncated) {
+    //     params2.Marker = response.NextMarker;
+    //   } else {
+    //     break;
+    //   }
+    // }
   }
 };
 
@@ -72,3 +153,4 @@ export default objectStorage;
 //     'testfile.png',
 //     '../models/erd-workbench.png'
 //   ))();
+//(async () => await objectStorage.getObjectlist(10))();
