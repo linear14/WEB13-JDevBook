76a179ead7b184ecc7c1bb9a978ec5ee2287c05c
LeeDongHyun <47495592+linear14@users.noreply.github.com>
2021-11-15 16:40:44 +0900

    fix: 이미지 1개 등록됐을 상황의 렌더링 로직 변경

--- client/src/utils/imageUtil.ts
+++ client/src/utils/imageUtil.ts
@@ -24,21 +24,16 @@ const imageUtil = {
   /**
    * 최적의 이미지 너비와 높이를 계산해서 반환해준다.
    * 각 너비와 높이는 target으로 지정한 값을 넘지 않도록 구성했다.
-   * @param target 컨테이너의 너비 혹은 높이. 새로운 너비와 높이는 이 값을 넘지 않는다.
+   * @param target 컨테이너의 모서리 길이(정사각형). 새로운 너비와 높이는 이 값을 넘지 않는다.
    * @param width 원본 너비
    * @param height 원본 높이
    * @returns [새로운 너비, 새로운 높이]
    */
   getImageFitSize: (target: number, width: number, height: number) => {
-    if (width === height) {
-      return [target, target];
-    }
-    if (width > height && width > target) {
+    if (width >= height) {
       return [target, Math.floor((height * target) / width)];
-    } else if (width < height && height > target) {
-      return [Math.floor((width * target) / height), target];
     } else {
-      return [width, height];
+      return [Math.floor((width * target) / height), target];
     }
   }
 };
