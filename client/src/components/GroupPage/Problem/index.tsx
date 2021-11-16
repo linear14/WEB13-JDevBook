587d54da4686c7f0dd194c70529ff74cd5aeb973
LeeDongHyun <47495592+linear14@users.noreply.github.com>
2021-11-16 21:33:41 +0900

    design: 문제 레이아웃 작업

--- client/src/components/GroupPage/Problem/index.tsx
+++ client/src/components/GroupPage/Problem/index.tsx
@@ -2,26 +2,77 @@ import React from 'react';
 import styled from 'styled-components';
 
 import palette from 'theme/palette';
+import style from 'theme/style';
 import { IProblem } from 'types/group';
 
 const ProblemContainer = styled.div`
   width: 680px;
   min-width: 680px;
-  height: 240px;
   position: relative;
   border-radius: 8px;
   box-sizing: border-box;
   box-shadow: rgba(0, 0, 0, 0.16) 0px 1px 5px;
   margin-top: 24px;
   background-color: ${palette.white};
+  padding-bottom: 24px;
 
   p {
     margin: 0;
   }
 `;
 
+const Body = styled.div`
+  font-size: ${style.font.large};
+  padding: ${style.padding.normal} ${style.padding.large} ${style.padding.large};
+  box-sizing: border-box;
+`;
+
+const AnswerWrap = styled.div`
+  display: flex;
+  justify-content: space-evenly;
+`;
+const RightButton = styled.div`
+  width: 180px;
+  height: 180px;
+  background: lightblue;
+  border-radius: 16px;
+`;
+
+const WrongButton = styled.div`
+  width: 180px;
+  height: 180px;
+  background: pink;
+  border-radius: 16px;
+`;
+
+const SolvedLabel = styled.div`
+  width: 100%;
+  height: 32px;
+  background: ${palette.darkgreen};
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-top-left-radius: 8px;
+  border-top-right-radius: 8px;
+  color: white;
+  letter-spacing: 1px;
+
+  &::after {
+    content: '풀었던 문제';
+  }
+`;
+
 const Problem = ({ problem }: { problem: IProblem }) => {
-  return <ProblemContainer>{problem.question}</ProblemContainer>;
+  return (
+    <ProblemContainer>
+      <SolvedLabel />
+      <Body>Q. {problem.question}</Body>
+      <AnswerWrap>
+        <RightButton />
+        <WrongButton />
+      </AnswerWrap>
+    </ProblemContainer>
+  );
 };
 
 export default Problem;
