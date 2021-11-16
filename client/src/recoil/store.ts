a71251f4ac2a24467da857af0398265d49cdf7dc
shinn338 <shinn338@naver.com>
2021-11-17 02:06:14 +0900

    feat: 알림창을 위한 recoil store 추가

--- client/src/recoil/store.ts
+++ client/src/recoil/store.ts
@@ -2,6 +2,7 @@ import socket from 'components/common/Socket';
 import { atom } from 'recoil';
 import { Socket } from 'socket.io-client';
 
+import { Alert } from 'types/common';
 import { PostData } from 'types/post';
 
 export const modalStateStore = atom({
@@ -109,5 +110,14 @@ export const GroupNavState = atom({
     about: true,
     problem: false,
     groupChat: false
+  }
+});
+
+export const AlertState = atom<Alert>({
+  key: 'AlertState',
+  default: {
+    bgColor: undefined,
+    comment: '',
+    modalState: false
   }
 });
