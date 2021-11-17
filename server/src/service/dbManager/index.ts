85806003e2d208759f7da7978ddd20be9414a830
idiot-kitto <46341496+idiot-kitto@users.noreply.github.com>
2021-11-17 16:03:54 +0900

    refactor: 나머지 dbManager 정리 ( comment, user, index )

--- server/src/service/dbManager/index.ts
+++ server/src/service/dbManager/index.ts
@@ -1,14 +1,14 @@
-import { Op } from 'sequelize';
 
 import db from '../../models';
 
 import { toggleLikePosts, updateLikeNum } from './like';
 import { getPosts, addPost, updatePost, deletePost } from './post';
-import { getComments } from './comment';
-import { getAllUsers, getUseridx, getUserName } from './user';
+import { getComments, addComment } from './comment';
+import { getUserData, getAllUsers, getUseridx, getUserName } from './user';
 import { searchUsers } from './search';
 import { getProblems, insertSolvedProblem } from './problem';
 import { CommentData } from '../../types/interface';
+import { setChatList, getChatList } from './chat';
 
 const dbManager = {
   sync: async () => {
@@ -21,74 +21,26 @@ const dbManager = {
         console.error('Unable to connect to the database:', error);
       });
   },
-
-  getUserdata: async (username: string) => {
-    const [user, created] = await db.models.User.findOrCreate({
-      include: db.models.Problem,
-      where: { nickname: username },
-      defaults: { nickname: username },
-      logging: false
-    });
-
-    return user.get();
-  },
-
-  searchUsers,
-
-  getPosts,
-  addPost,
-  updatePost,
-  deletePost,
-
+  
+  getUserData,
   getAllUsers,
   getUserName,
   getUseridx,
 
-  getChatList: async function (sender: string, receiver: string) {
-    const senderidx: number = await this.getUseridx(sender);
-    const receiveridx: number = await this.getUseridx(receiver);
+  searchUsers,
+  
+  getPosts,
+  addPost,
+  updatePost,
+  deletePost,  
 
-    const allChats = await db.models.Chat.findAll({
-      where: {
-        [Op.or]: [
-          { senderidx: senderidx, receiveridx: receiveridx },
-          { senderidx: receiveridx, receiveridx: senderidx }
-        ]
-      },
-      logging: false
-    });
-    const allChatsArray = allChats.map((data: any) => data.get());
-
-    return {
-      senderidx: senderidx,
-      receiveridx: receiveridx,
-      previousMsg: allChatsArray
-    };
-  },
-
-  setChatList: async function (sender: string, receiver: string, msg: string) {
-    const senderidx: number = await this.getUseridx(sender);
-    const receiveridx: number = await this.getUseridx(receiver);
-    await db.models.Chat.create({
-      senderidx: senderidx,
-      receiveridx: receiveridx,
-      content: msg,
-      logging: false
-    });
-  },
-
-  addComment: async function (addComment: CommentData) {
-    const userIdx: number = await this.getUseridx(addComment.sender);
-    const result = await db.models.Comment.create({
-      ...addComment,
-      useridx: userIdx
-    });
-    return result.get();
-  },
-
+  setChatList,
+  getChatList,
+  
   toggleLikePosts,
   updateLikeNum,
-
+  
+  addComment,
   getComments,
 
   getProblems,
--- server/src/service/dbManager/user.ts
+++ server/src/service/dbManager/user.ts
@@ -1,26 +1,36 @@
 import db from '../../models';
-import './index';
+
+const getUserData = async (username: string) => {
+    const [user, created] = await db.models.User.findOrCreate({
+      include: db.models.Problem,
+      where: { nickname: username },
+      defaults: { nickname: username },
+      logging: false
+    });
+
+    return user.get();
+  }
 
 const getAllUsers = async () => {
-    const users = await db.models.User.findAll({ logging: false });
-    return users;
-  }
+  const users = await db.models.User.findAll({ logging: false });
+  return users;
+};
 
 const getUserName = async function (idx: number) {
-    const username = await db.models.User.findOne({
-      where: { idx: idx },
-      logging: false
-    });
-    return username?.get().nickname;
-  }
+  const username = await db.models.User.findOne({
+    where: { idx: idx },
+    logging: false
+  });
+  return username?.get().nickname;
+};
 
 const getUseridx = async function (name: string) {
-    const user = await db.models.User.findOne({
-      where: { nickname: name },
-      logging: false
-    });
+  const user = await db.models.User.findOne({
+    where: { nickname: name },
+    logging: false
+  });
 
-    return user?.get().idx ? user?.get().idx : -1;
-  }
+  return user?.get().idx ? user?.get().idx : -1;
+};
 
-  export { getAllUsers, getUserName, getUseridx };
+export { getUserData, getAllUsers, getUserName, getUseridx };
