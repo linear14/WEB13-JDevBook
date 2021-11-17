4321103238e0d573b8968894aacafd02cfaa431a
shinn338 <shinn338@naver.com>
2021-11-17 22:00:16 +0900

    feat: fetch를 이용해 그룹 리스트를 불러와 출력
    fetch를 이용해서 그룹 리스트를 불러오고 각각의 그룹 카드를 생성하여 표시
    
    Resolves: #116

--- client/src/components/GroupSelectPage/GroupSelectList/index.tsx
+++ client/src/components/GroupSelectPage/GroupSelectList/index.tsx
@@ -1,9 +1,12 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import styled from 'styled-components';
 import { useRecoilState, useRecoilValue } from 'recoil';
 
 import { rightModalStates, groupListState } from 'recoil/store';
+import fetchApi from 'api/fetch';
+
 import GroupCard from 'components/GroupSelectPage/GroupSelectList/GroupCard';
+import { IGroup } from 'types/group';
 
 const GroupSelectListContainer = styled.div<{ modalState: boolean }>`
   width: ${(props) => (props.modalState ? `100%` : `130%`)};
@@ -18,10 +21,19 @@ const GroupSelectList = () => {
   const rightModalState = useRecoilValue(rightModalStates).rightModalFlag;
   const [groupList, setGroupList] = useRecoilState(groupListState);
 
+  const fetchList = async () => {
+    const groupList: IGroup[] = await fetchApi.getGroupList();
+    setGroupList(groupList);
+  };
+
+  useEffect(() => {
+    fetchList();
+  }, []);
+
   return (
     <GroupSelectListContainer modalState={rightModalState}>
       {groupList.map((group) => (
-        <GroupCard group={group} />
+        <GroupCard key={group.idx} group={group} />
       ))}
     </GroupSelectListContainer>
   );
