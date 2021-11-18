a220b122025c76e84d121cfc2d5a6087fa51030d
shinn338 <shinn338@naver.com>
2021-11-18 21:58:01 +0900

    refactor: 서버단 상대경로로 변경

--- server/src/service/dbManager/comment.ts
+++ server/src/service/dbManager/comment.ts
@@ -1,4 +1,4 @@
-import { CommentData } from 'types/interface';
+import { CommentData } from '../../types/interface';
 import db from '../../models';
 import './index';
 
@@ -22,12 +22,12 @@ const addComment = async function (addComment: CommentData) {
     logging: false
   });
   const userIdx: number = user?.get().idx ? user?.get().idx : -1;
-  
+
   const result = await db.models.Comment.create({
     ...addComment,
     useridx: userIdx
   });
   return result.get();
-}
+};
 
 export { getComments, addComment };
