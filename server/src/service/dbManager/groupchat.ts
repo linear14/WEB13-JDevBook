e0d2e03fc351e04dd424603e0bbc465e6d362c87
idiot-kitto <46341496+idiot-kitto@users.noreply.github.com>
2021-11-18 18:33:52 +0900

    feat: 소켓으로 채팅 받아 groupchat 테이블에 삽입하는 기능 추가

--- server/src/service/dbManager/groupchat.ts
+++ server/src/service/dbManager/groupchat.ts
@@ -1,3 +1,25 @@
 import db from '../../models';
 
-export {};
+const setGroupChatList = async (
+  sender: string,
+  groupidx: number,
+  msg: string
+) => {
+  const senderData = await db.models.User.findOne({
+    where: { nickname: sender },
+    logging: false
+  });
+
+  const senderidx: number = senderData?.get().idx ? senderData?.get().idx : -1;
+
+  await db.models.GroupChat.create(
+    {
+      useridx: senderidx,
+      groupidx: groupidx,
+      content: msg
+    },
+    { logging: false }
+  );
+};
+
+export { setGroupChatList };
