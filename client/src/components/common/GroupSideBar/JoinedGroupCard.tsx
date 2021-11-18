7334eb757cde1ce115803901550ebd4e6c5a04c8
shinn338 <shinn338@naver.com>
2021-11-18 20:08:09 +0900

    feat: 그룹 리스트바 클릭시 해당 그룹페이지로 이동 구현
    Related to: #55

---
+++ client/src/components/common/GroupSideBar/JoinedGroupCard.tsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+
+import { defaultGroup } from 'images/groupimg';
+import { IGroup } from 'types/group';
+import palette from 'theme/palette';
+
+const JoinedGroupCardWrap = styled(Link)`
+  display: flex;
+  align-items: center;
+  text-decoration: none;
+  color: ${palette.black};
+  padding: 5px;
+  margin: 0 0 20px 0;
+  font-weight: bold;
+
+  img {
+    width: 50px;
+    height: 50px;
+    margin: 0 20px 0 0;
+    border-radius: 10px;
+    object-fit: cover;
+  }
+
+  &:hover {
+    border-radius: 10px;
+    background: ${palette.lightgray};
+    transition: all 0.2s;
+  }
+
+  &:active {
+    background: ${palette.gray};
+  }
+`;
+
+const JoinedGroupCard = ({ group }: { group: IGroup }) => {
+  const url = `/group/${group.idx}`;
+
+  return (
+    <JoinedGroupCardWrap to={url}>
+      <img src={group.cover || defaultGroup} alt="cover 아이콘" />
+      <p>{group.title}</p>
+    </JoinedGroupCardWrap>
+  );
+};
+
+export default JoinedGroupCard;
---
+++ client/src/components/common/GroupSideBar/index.tsx
@@ -0,0 +1,86 @@
+import React, { useEffect, useState } from 'react';
+import styled from 'styled-components';
+import { useRecoilValue } from 'recoil';
+
+import { groupListState, myJoinedGroupState } from 'recoil/store';
+import palette from 'theme/palette';
+import { iconSearch } from 'images/icons';
+import { IGroup } from 'types/group';
+
+import JoinedGroupCard from './JoinedGroupCard';
+
+const GroupSideBarContainer = styled.div`
+  flex: 1;
+  width: inherit;
+  background: ${palette.white};
+  display: flex;
+  flex-direction: column;
+  box-shadow: rgba(0, 0, 0, 0.24) 3px 3px 3px;
+`;
+
+const SearchBarWrap = styled.div`
+  height: 40px;
+  margin: 30px 50px;
+  display: flex;
+  background: ${palette.lightgray};
+  border-radius: 24px;
+
+  img {
+    width: 20px;
+    height: 20px;
+    margin: 10px 5px 10px 15px;
+  }
+
+  input {
+    width: 70%;
+    outline: none;
+    background: none;
+    border: none;
+    font-size: 1rem;
+  }
+`;
+
+const GroupList = styled.ul`
+  flex: 1;
+  margin: 0 50px 30px 0;
+  display: flex;
+  flex-direction: column;
+`;
+
+const GroupSideBar = () => {
+  const groupList = useRecoilValue(groupListState);
+  const joinedGroupIdx = useRecoilValue(myJoinedGroupState);
+  const [joinedGroup, setJoinedGroup] = useState<IGroup[]>([]);
+  const [searchGroup, setSearchGroup] = useState<IGroup[]>([]);
+
+  const searchHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchGroup(
+      joinedGroup.filter((group) => group.title.includes(e.target.value))
+    );
+  };
+
+  useEffect(() => {
+    setJoinedGroup(
+      groupList.filter((group) => joinedGroupIdx.includes(group.idx))
+    );
+    setSearchGroup(
+      groupList.filter((group) => joinedGroupIdx.includes(group.idx))
+    );
+  }, [joinedGroupIdx]);
+
+  return (
+    <GroupSideBarContainer className="no-drag">
+      <SearchBarWrap>
+        <img src={iconSearch} alt="Search 아이콘" />
+        <input type="text" placeholder="그룹 검색" onChange={searchHandler} />
+      </SearchBarWrap>
+      <GroupList>
+        {searchGroup.map((searchGroup, idx) => (
+          <JoinedGroupCard key={idx} group={searchGroup} />
+        ))}
+      </GroupList>
+    </GroupSideBarContainer>
+  );
+};
+
+export default GroupSideBar;
