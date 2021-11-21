4ea5c453f9facb38670a104442e26de170b8fce7
shinn338 <shinn338@naver.com>
2021-11-22 00:01:56 +0900

    feat: ProfilePhoto에 해당 프로필 페이지로의 Link 추가

--- client/src/components/common/ProfilePhoto.tsx
+++ client/src/components/common/ProfilePhoto.tsx
@@ -1,25 +1,35 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 
 import { ProfilePhotoProps } from 'types/common';
 import { defaultProfile } from 'images';
 
+const ProfilePhotoWrap = styled(Link)`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
 const StyledProfilePhoto = styled.img<ProfilePhotoProps>`
-  width: ${(props) => props.size || '65px'};
-  height: ${(props) => props.size || '65px'};
+  width: ${(props) => props.size || '70px'};
+  height: ${(props) => props.size || '70px'};
   border-radius: 50%;
 `;
 
 const ProfilePhoto = ({ userName, size }: ProfilePhotoProps) => {
   const profileImgURL = `https://github.com/${userName}.png`;
+  const profileURL = `/profile/${userName}`;
 
   return (
-    <StyledProfilePhoto
-      src={userName !== '' ? profileImgURL : defaultProfile}
-      size={size}
-      alt="프로필 사진"
-      className="no-drag"
-    ></StyledProfilePhoto>
+    <ProfilePhotoWrap to={profileURL}>
+      <StyledProfilePhoto
+        src={userName !== '' ? profileImgURL : defaultProfile}
+        size={size}
+        alt="프로필 사진"
+        className="no-drag"
+      />
+    </ProfilePhotoWrap>
   );
 };
 
