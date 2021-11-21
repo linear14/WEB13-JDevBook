9e5644cf4d45b77df8c848cd87e0d10abfd78a70
shinn338 <shinn338@naver.com>
2021-11-21 23:48:29 +0900

    refactor: 불필요한 코드 제거

--- client/src/images/index.tsx
+++ client/src/images/index.tsx
@@ -1,7 +1,5 @@
-// import profileDefault from 'images/profile-default.png';
 import defaultProfile from 'images/default-profile.jpg';
-import defaultCover from 'images/default-profile.jpg';
 import mainLogo from 'images/main-logo.png';
 import errorImage from 'images/error-image.png';
 
-export { mainLogo, defaultProfile, defaultCover, errorImage };
+export { mainLogo, defaultProfile, errorImage };
