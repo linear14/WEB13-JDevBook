5237aa46342f2b584246dc0ea22a1b8bee819916
LeeDongHyun <47495592+linear14@users.noreply.github.com>
2021-11-22 18:39:36 +0900

    feat: 새로운 게시글 알림 클릭하여 reload 이벤트 동작하도록 구현

--- client/src/components/HomePage/PostList.tsx
+++ client/src/components/HomePage/PostList.tsx
@@ -55,7 +55,7 @@ const PostList = () => {
     const fetchPosts = await fetchApi.getPosts();
     const fetchProblems = await fetchApi.getProblems();
     const result = await Promise.all([fetchPosts, fetchProblems]);
-    setPosts((prev) => prev.concat(result[0]));
+    setPosts(result[0]);
     setProblems(result[1]);
     problemOrders.current = arrayUtil.shuffle(
       Array(result[1].length)
@@ -86,9 +86,9 @@ const PostList = () => {
   useEffect(() => {
     fetchInit();
 
-    return (() => {
+    return () => {
       setPosts([]);
-    })
+    };
   }, []);
 
   const getNextProblem = (idx: number) => {
@@ -98,9 +98,16 @@ const PostList = () => {
       : null;
   };
 
+  const reloadList = () => {
+    window.scrollTo({ top: 0 });
+    setPosts([]);
+    setProblems([]);
+    fetchInit();
+  };
+
   return (
     <>
-      <NewPostAlert />
+      <NewPostAlert reloadList={reloadList} />
       <PostListContainer>
         {Array(posts.length)
           .fill(undefined)
