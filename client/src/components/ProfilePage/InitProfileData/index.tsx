fccde80a795a739e39acf3bbc6fbad06b70321da
hsy <reservedcalender@gmail.com>
2021-11-23 16:05:00 +0900

    fix: InitProfileData 에서 recoil 전역 형식 맞춰주기
    Related to: #145 #148 #152

--- client/src/components/ProfilePage/InitProfileData/index.tsx
+++ client/src/components/ProfilePage/InitProfileData/index.tsx
@@ -6,18 +6,20 @@ import fetchApi from 'api/fetch';
 import { IProfile } from 'types/user';
 
 const InitProfileData = ({ userName }: { userName: string }) => {
-  const [profileData, setprofileData] = useRecoilState(profileState);
+  const [profileData, setProfileData] = useRecoilState(profileState);
   const resetprofileData = useResetRecoilState(profileState);
 
   const fetchProfile = async (userName: string) => {
     const { data: profile, error } = await fetchApi.getProfile(userName);
-    if (!error) setprofileData(profile);
-    //       setProfileData({
-    //         idx: profile.idx,
-    //         nickname: profile.nickname,
-    //         cover: profile.cover,
-    //         bio: profile.bio
-    //       });
+    //if (!error) setprofileData(profile);
+    if (!error) {
+      setProfileData({
+        idx: profile.idx,
+        nickname: profile.nickname,
+        cover: profile.cover,
+        bio: profile.bio
+      });
+    }
   };
 
   useEffect(() => {
