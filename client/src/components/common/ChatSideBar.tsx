61728f684d90cc200a52c30a063e57777faadbcc
idiot-kitto <46341496+idiot-kitto@users.noreply.github.com>
2021-11-23 14:21:02 +0900

    design: chatsidebar margin 적용 오류 수정

--- client/src/components/common/ChatSideBar.tsx
+++ client/src/components/common/ChatSideBar.tsx
@@ -179,10 +179,8 @@ const Divider = styled.div`
   width: calc(100% - 32px);
   height: 1px;
   background: #dddddd;
-  margin: (
-    ${style.margin.normal} ${style.margin.large} ${style.margin.normal}
-      ${style.margin.large}
-  );
+  margin: ${style.margin.normal} ${style.margin.large} ${style.margin.normal}
+    ${style.margin.large};
 `;
 
 const ChatSideBar = () => {
