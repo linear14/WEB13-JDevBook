1add9876e054b3aba928f4dd139e67ad6d4683b8
hsy <reservedcalender@gmail.com>
2021-11-30 23:08:10 +0900

    feat: 로그인 페이지 전 로딩 페이지 작성
    Related to: #190

---
+++ client/src/pages/LoadingWhitePage.tsx
@@ -0,0 +1,29 @@
+import { useEffect, useState } from 'react';
+import { useHistory } from 'react-router-dom';
+import styled, { keyframes } from 'styled-components';
+
+const Content = styled.div`
+  width: 100%;
+  height: 100%;
+  background-color: white;
+`;
+
+const LoadingWhitePage = ({ login }: { login: boolean }) => {
+  const history = useHistory();
+  const [check, setCheck] = useState(false);
+
+  useEffect(() => {
+    if (check === true && login === true) {
+      history.goBack();
+    }
+    setCheck(true);
+  }, [login]);
+
+  return (
+    <>
+      <Content></Content>
+    </>
+  );
+};
+
+export default LoadingWhitePage;
