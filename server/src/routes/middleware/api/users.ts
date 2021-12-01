3693f27927e47f6a199187fb265f3aae59535c49
hsy <reservedcalender@gmail.com>
2021-12-01 22:42:56 +0900

    refactor: routes 및 middleware 변경
    isLogin과 users 모듈 변경. userData를 users로 옮김
    
    Related to: #143

--- server/src/routes/middleware/api/users.ts
+++ server/src/routes/middleware/api/users.ts
@@ -1,5 +1,9 @@
 import { Request, Response, NextFunction } from 'express';
+import jwt from 'jsonwebtoken';
+
+import { DBUser } from '../../../types/interface';
 import dbManager from '../../../service/dbManager';
+const oauth = require('../../../config/oauth.json');
 
 const users = {
   search: async (
@@ -23,6 +27,29 @@ const users = {
     } catch (err) {
       console.error(err);
       res.json([]);
+    }
+  },
+  data: async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const { name } = jwt.verify(req.session.jwt, oauth.jwtKey) as {
+        name: string;
+      };
+      if (name === req.session.username) {
+        const userdata: DBUser = await dbManager.getUserData(name);
+        res.json({
+          data: userdata,
+          error: false
+        });
+      } else {
+        console.log(`로그인 정보 비정상 감지: ${req.session.username}`);
+        req.session.destroy((err) => {
+          res.json({ data: '', error: true });
+        });
+      }
+    } catch (err) {
+      req.session.destroy((err) => {
+        res.json({ data: '', error: true });
+      });
     }
   }
 };
